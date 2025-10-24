3962e3f3964f810ed073b83cab5da69ebea18da7
trevor-poirier <117388844+trevor-poirier@users.noreply.github.com>
2025-10-24 12:03:36 -0500

    Add files via upload

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ kubectl create ns echo-sound
 kubectl apply -f ./CKA-Exam-Questions/Q4/deploy.yaml
 kubectl apply -f ./CKA-Exam-Questions/Q4/svc.yaml
 cat ./CKA-Exam-Questions/Q4/README.md
+printf '\n'
