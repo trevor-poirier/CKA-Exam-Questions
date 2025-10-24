51bbfa2ad19803661d938c3bdc19976b7683614a
trevor-poirier <117388844+trevor-poirier@users.noreply.github.com>
2025-10-24 13:22:57 -0500

    Delete Q4/init.sh

--- Q4/init.sh
+++
@@ -1,5 +0,0 @@
-# Q4
-kubectl create ns echo-sound
-kubectl apply -f ./CKA-Exam-Questions/Q4/deploy.yaml
-kubectl apply -f ./CKA-Exam-Questions/Q4/svc.yaml
-cat ./CKA-Exam-Questions/Q4/README.md; printf '\n'
