ab3299ea92b76e43009b94f46831cd42c4dd9b40
trevor-poirier <117388844+trevor-poirier@users.noreply.github.com>
2025-10-24 13:35:15 -0500

    Add files via upload

--- init.sh
+++ init.sh
@@ -10,6 +10,6 @@ cat ./CKA-Exam-Questions/Q4/README.md
 printf '\n'
 
 # Last thing to run from Q1 because it will break cluster
-DIR=$(shuf -i 0-5 -n 1)
+DIR=$(shuf -i 1-8 -n 1)
 FILE=$(ls CKA-Exam-Questions/Q1/Scenarios/$DIR)
 cp -f CKA-Exam-Questions/Q1/Scenarios/$DIR/$FILE /etc/kubernetes/manifests/$FILE
