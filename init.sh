2b283bb9f2f31ab7a395b8683e93af7e26d239d5
trevor-poirier <117388844+trevor-poirier@users.noreply.github.com>
2025-10-27 17:03:32 -0500

    Add files via upload

--- init.sh
+++ init.sh
@@ -1,99 +1,82 @@
 # Q1 - Question only
-cat ./CKA-Exam-Questions/Q1/README.md
-printf '\n'
+printf '\nQ1\n'
 
-# Q2
-sudo rm /etc/cni/net.d/calico-kubeconfig
-for crd in $(kubectl get crds -o name | grep "projectcalico.org"); do kubectl delete $crd; done
-kubectl delete deploy,ds,cm -l k8s-app=calico-kube-controllers -n kube-system
-kubectl delete pod -l k8s-app=calico-kube-controllers -n kube-system --force
-cat ./CKA-Exam-Questions/Q2/README.md
-printf '\n'
+# Q2 - Question only
+printf '\nQ2\n'
 
 # Q3
-cat ./CKA-Exam-Questions/Q3/README.md
-printf '\n'
+printf '\nQ3\n'
 
 # Q4
+printf '\nQ4\n'
 kubectl create ns autoscale
 kubectl apply -f ./CKA-Exam-Questions/Q4/deploy.yaml
 kubectl apply -f ./CKA-Exam-Questions/Q4/svc.yaml
 cat ./CKA-Exam-Questions/Q4/README.md
-printf '\n'
 
 # Q5
+printf '\nQ5\n'
 kubectl create ns nginx-static
 kubectl apply -f ./CKA-Exam-Questions/Q5/deploy.yaml
 kubectl apply -f ./CKA-Exam-Questions/Q5/svc.yaml
 kubectl apply -f ./CKA-Exam-Questions/Q5/cm.yaml
 cat ./CKA-Exam-Questions/Q5/README.md
-printf '\n'
 
 # Q6
+printf '\nQ6\n'
 kubectl create ns echo-sound
 kubectl apply -f ./CKA-Exam-Questions/Q6/deploy.yaml
 kubectl apply -f ./CKA-Exam-Questions/Q6/svc.yaml
-cat ./CKA-Exam-Questions/Q6/README.md
-printf '\n'
 
 # Q7
+printf '\nQ7\n'
 kubectl create ns argocd
 kubectl apply -k https://github.com/argoproj/argo-cd/manifests/crds\?ref\=stable
-cat ./CKA-Exam-Questions/Q7/README.md
-printf '\n'
 
 # Q8
+printf '\nQ8\n'
 kubectl apply -f ./CKA-Exam-Questions/Q8/deploy.yaml
-cat ./CKA-Exam-Questions/Q8/README.md
-printf '\n'
 
 # Q9
+printf '\nQ9\n'
 kubectl create ns mariadb
 kubectl apply -f ./CKA-Exam-Questions/Q9/pv.yaml
-cat ./CKA-Exam-Questions/Q9/README.md
-printf '\n'
-
 
 # Q10
+printf '\nQ10\n'
 kubectl create ns relative-fawn
 kubectl apply -f ./CKA-Exam-Questions/Q10/deploy.yaml
-cat ./CKA-Exam-Questions/Q10/README.md
-printf '\n'
 
 # Q11
-cat ./CKA-Exam-Questions/Q11/README.md
-printf '\n'
+printf '\nQ11\n'
 
 # Q12
+printf '\nQ12\n'
 kubectl create ns spline-reticulator
 kubectl apply -f ./CKA-Exam-Questions/Q12/deploy.yaml
-cat ./CKA-Exam-Questions/Q12/README.md
-printf '\n'
 
 # Q13
+printf '\nQ13\n'
 kubectl create ns priority
 kubectl create priorityclass user-max-priority --value=1000000000
 kubectl apply -f ./CKA-Exam-Questions/Q13/deploy.yaml
-cat ./CKA-Exam-Questions/Q13/README.md
-printf '\n'
 
 # Q14
+printf '\nQ14\n'
 kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.0/standard-install.yaml
 kubectl create secret tls web-tls --cert=./CKA-Exam-Questions/Q14/tls.crt --key=./CKA-Exam-Questions/Q14/tls.key -n default
 kubectl apply -f ./CKA-Exam-Questions/Q14/deploy.yaml
 kubectl apply -f ./CKA-Exam-Questions/Q14/svc.yaml
 kubectl apply -f ./CKA-Exam-Questions/Q14/ingress.yaml
 kubectl apply -f ./CKA-Exam-Questions/Q14/gateway-class.yaml
-cat ./CKA-Exam-Questions/Q14/README.md
-printf '\n'
 
 # Q15
+printf '\nQ15\n'
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.19.1/cert-manager.yaml
 kubectl delete mutatingwebhookconfiguration,validatingwebhookconfiguration,deploy,clusterrolebinding,clusterrole,rolebinding,role,svc,serviceaccount -l app.kubernetes.io/instance=cert-manager -n cert-manager
-cat ./CKA-Exam-Questions/Q15/README.md
-printf '\n'
 
 # Q16
+printf '\nQ16\n'
 kubectl create ns frontend
 kubectl create ns backend
 kubectl create ns database
@@ -102,8 +85,12 @@ kubectl label namespace backend namespace=backend
 kubectl label namespace database namespace=database
 kubectl apply -f ./CKA-Exam-Questions/Q16/deploy.yaml
 kubectl apply -f ./CKA-Exam-Questions/Q16/svc.yaml
-cat ./CKA-Exam-Questions/Q16/README.md
-printf '\n'
+
+# Second to last thing to run from Q2 before breaking cluster
+sudo rm /etc/cni/net.d/calico-kubeconfig
+for crd in $(kubectl get crds -o name | grep "projectcalico.org"); do kubectl delete $crd; done
+kubectl delete deploy,ds,cm -l k8s-app=calico-kube-controllers -n kube-system
+kubectl delete pod -l k8s-app=calico-kube-controllers -n kube-system --force
 
 # Last thing to run from Q1 because it will break cluster
 DIR=$(shuf -i 1-8 -n 1)
