e9e343f0a91a0c4413b41b1e51a55d96d491bb5b
trevor-poirier <117388844+trevor-poirier@users.noreply.github.com>
2025-11-05 14:07:49 -0600

    Add files via upload

--- QUESTIONS.sh
+++ QUESTIONS.sh
@@ -21,5 +21,5 @@ cat ./CKA-Exam-Questions/Q15/README.md
 cat ./CKA-Exam-Questions/Q16/README.md
 
 # Command to copy to re-print a question
-printf '\nTo review a specific question later on, run this command below.\nWhere "Q#" is the letter Q followed by the question number (i.e. Q12):\n\ncat ./CKA-Exam-Questions/Q#/README.md\n\n'
+printf '\nTo review a specific question later on, run this command below.\nWhere "Q#" is the letter Q followed by the question number (i.e. Q12):\n\ncat ./CKA-Exam-Questions/Q#/README.md\n'
 printf '\nTo list all questions again, run sh CKA-Exam-Questions/QUESTIONS.sh\n\n'
