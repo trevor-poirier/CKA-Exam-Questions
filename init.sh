5b6c188b50185420172ad0d02fde7680de93f04c
trevor-poirier <117388844+trevor-poirier@users.noreply.github.com>
2025-11-06 14:49:25 -0600

    Add files via upload

--- init.sh
+++ init.sh
@@ -87,6 +87,7 @@ kubectl apply -f ./CKA-Exam-Questions/Q16/svc.yaml
 
 # From question number 6 - Installing last to avoid errors with Ingress creation earlier on
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.14.0/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f ./CKA-Exam-Questions/Q6/ingress-controller.yaml -n ingress-nginx
 
 echo "First Argument: $1"
 
